582e217dfd618acbe46d3b1f87f27e115e63132d
WangSL <wangshilong0809@outlook.com>
2024-03-11 23:16:01 +0800

    bug fix

--- api/get_all_rooms.py
+++ api/get_all_rooms.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import text
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
+from starlette import status
 
+from auth_schema import User
 from database import get_db
+from main import get_current_active_user
 
 router = APIRouter()
 
@@ -16,17 +19,24 @@ class GetAllRoomsResp(BaseModel):
 
 
 @router.get("/get_all_rooms")
-def get_all_room_info(db: Session = Depends(get_db)):
-    sql = text(
-        """
-        SELECT *
-        FROM room
-        JOIN client ON room.client_id = client.id
-        LEFT JOIN meal_plan ON client.meal_plan_id = meal_plan.meal_plan_id
-        LEFT JOIN recovery_plan ON client.recovery_plan_id = recovery_plan.recovery_plan_id
-        LEFT JOIN baby_nurse ON client.assigned_baby_nurse = baby_nurse.baby_nurse_id
-        """
-    )
-    result = db.execute(sql)
-    rooms_info = [dict(row) for row in result.mappings()]
-    return rooms_info
+def get_all_room_info(current_user: User = Depends(get_current_active_user), db: Session = Depends(get_db)):
+    if current_user.role == "admin":
+        sql = text(
+            """
+            SELECT *
+            FROM room
+            JOIN client ON room.client_id = client.id
+            LEFT JOIN meal_plan ON client.meal_plan_id = meal_plan.meal_plan_id
+            LEFT JOIN recovery_plan ON client.recovery_plan_id = recovery_plan.recovery_plan_id
+            LEFT JOIN baby_nurse ON client.assigned_baby_nurse = baby_nurse.baby_nurse_id
+            """
+        )
+        result = db.execute(sql)
+        rooms_info = [dict(row) for row in result.mappings()]
+        return rooms_info
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
