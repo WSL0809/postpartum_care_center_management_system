25c04589319800753e3d37d793623b741c3b5dd2
WangSL <48207171+WSL0809@users.noreply.github.com>
2024-03-12 15:49:57 +0800

    get_all_rooms.py

--- api/get_all_rooms.py
+++ api/get_all_rooms.py
@@ -41,7 +41,7 @@ def get_all_room_info(current_user: User = Depends(get_current_active_user), db:
             recovery_plan.details AS recovery_plan_details, recovery_plan.duration AS recovery_plan_duration,
             room.status AS status, room.recently_used AS recently_used, room.notes AS notes
             FROM room
-            JOIN client ON room.client_id = client.id
+            LEFT JOIN client ON room.client_id = client.id
             LEFT JOIN meal_plan ON client.meal_plan_id = meal_plan.meal_plan_id
             LEFT JOIN recovery_plan ON client.recovery_plan_id = recovery_plan.recovery_plan_id
             LEFT JOIN baby_nurse ON client.assigned_baby_nurse = baby_nurse.baby_nurse_id
