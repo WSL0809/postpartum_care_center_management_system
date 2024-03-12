584273c64a1904fc6df18a800fd4f6640e3c9e44
WangSL <48207171+WSL0809@users.noreply.github.com>
2024-03-12 13:45:00 +0800

    reserve.py

--- api/get_all_rooms.py
+++ api/get_all_rooms.py
@@ -7,12 +7,28 @@ from starlette import status
 from auth import get_current_active_user
 from auth_schema import User
 from database import get_db
+from model import Room, Client
+from model.client import BabyNurse
 
 router = APIRouter()
 
 
+class MealPlan(BaseModel):
+    details: str
+    duration: int
+
+
+class RecoveryPlan(BaseModel):
+    details: str
+    duration: int
+
+
 class GetAllRoomsResp(BaseModel):
-    id: int
+    room_number: str
+    name: str
+    baby_nurse_name: str
+    meal_plan: MealPlan
+    recovery_plan: RecoveryPlan
     status: str
     recently_used: str
     notes: str
@@ -23,7 +39,12 @@ def get_all_room_info(current_user: User = Depends(get_current_active_user), db:
     if current_user.role == "admin":
         sql = text(
             """
-            SELECT *
+            SELECT room_number,
+            client.name,
+            baby_nurse.name,
+            meal_plan.details, meal_plan.duration,
+            recovery_plan.details, recovery_plan.duration,
+            room.status, room.recently_used, room.notes
             FROM room
             JOIN client ON room.client_id = client.id
             LEFT JOIN meal_plan ON client.meal_plan_id = meal_plan.meal_plan_id
@@ -31,8 +52,28 @@ def get_all_room_info(current_user: User = Depends(get_current_active_user), db:
             LEFT JOIN baby_nurse ON client.assigned_baby_nurse = baby_nurse.baby_nurse_id
             """
         )
-        result = db.execute(sql)
-        rooms_info = [dict(row) for row in result.mappings()]
+        result = db.execute(sql).fetchall()
+        print(result)
+        rooms_info = [
+            GetAllRoomsResp(
+                room_number=row[0],
+                name=row[1],
+                baby_nurse_name=row[2],
+                meal_plan=MealPlan(
+                    details=row[3],
+                    duration=row[4]
+                ),
+                recovery_plan=RecoveryPlan(
+                    details=row[5],
+                    duration=row[6]
+                ),
+                status=row[7],
+                recently_used=row[8],
+                notes=row[9]
+            ) for row in result
+        ]
+        result = db.execute(sql).mappings().all()
+        print(result)
         return rooms_info
     else:
         raise HTTPException(
--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from enum import Enum
 
 class RoomStatus(Enum):
     # 空闲的房间
-    Free = "1"
+    Free = "0"
     # 被占用的房间
     Occupied = "2"
     # 维修的房间
     Repair = "3"
     # 已预约的房间
-    Booked = "4"
+    Booked = "1"
