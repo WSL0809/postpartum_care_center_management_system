028df4a272add94fd920ee5fc456adf028cd704e
WangSL <wangshilong0809@outlook.com>
2024-03-21 05:21:20 +0800

    故障登记

--- api/fault_registration.py
+++ api/fault_registration.py
@@ -51,7 +51,6 @@ def fault_registration(fault_registration: FaultRegistrationRecv, current_user: 
         return FaultRegistrationResp(status=status.HTTP_401_UNAUTHORIZED, details="权限不足")
 
     if fault_registration.fault_list is None:
-        print("fault_registration is None")
         repair_complete(fault_registration.room_number, fault_registration.fault_list, db)
         return FaultRegistrationResp(status=status.HTTP_200_OK, details="修理完成")
 
