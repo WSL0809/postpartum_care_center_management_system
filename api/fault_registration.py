dad76ce2a1b2c5ba5a84cf2ca4513d53426e8825
WangSL <wangshilong0809@outlook.com>
2024-03-21 05:26:06 +0800

    故障登记

--- api/fault_registration.py
+++ api/fault_registration.py
@@ -50,7 +50,7 @@ def fault_registration(fault_registration: FaultRegistrationRecv, current_user: 
     if current_user.role != "admin":
         return FaultRegistrationResp(status=status.HTTP_401_UNAUTHORIZED, details="权限不足")
 
-    if fault_registration.fault_list is None:
+    if fault_registration.fault_list == '' or fault_registration.fault_list is None:
         repair_complete(fault_registration.room_number, db)
         return FaultRegistrationResp(status=status.HTTP_200_OK, details="修理完成")
 
