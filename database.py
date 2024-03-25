d40da2e38969be508b47e62b0ecda86e396ba509
WangSL <wangshilong0809@outlook.com>
2024-03-25 22:37:11 +0800

    Update database.py

--- database.py
+++ database.py
@@ -4,9 +4,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 
-# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@db.yun.store:23778/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@db.yun.store:23778/postgres"
 # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@127.0.0.1:5432/postgres"
-SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", "postgresql://postgres:postgres@db:5432/postgres")
+# SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", "postgresql://postgres:postgres@db:5432/postgres")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
