5c2b973aee6a1712d0cfef2c46177538d0695b35
WangSL <wangshilong0809@outlook.com>
2024-03-25 22:26:42 +0800

    bug fix

--- api/get_all_rooms.py
+++ api/get_all_rooms.py
@@ -47,6 +47,7 @@ def get_all_room_info(current_user: User = Depends(get_current_active_user), db:
             LEFT JOIN meal_plan ON client.meal_plan_id = meal_plan.meal_plan_id
             LEFT JOIN recovery_plan ON client.recovery_plan_id = recovery_plan.recovery_plan_id
             LEFT JOIN baby_nurse ON client.assigned_baby_nurse = baby_nurse.baby_nurse_id
+            WHERE client.status <> 1
             """
         )
         result = db.execute(sql).mappings().all()
