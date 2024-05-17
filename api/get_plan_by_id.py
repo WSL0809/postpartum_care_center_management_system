92fa4a9da20570c072ebeb0b44f4077bd807e8c4
WangSL <48207171+WSL0809@users.noreply.github.com>
2024-05-17 10:04:19 +0800

    bugfix

--- api/get_plan_by_id.py
+++ api/get_plan_by_id.py
@@ -2,16 +2,24 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 import model
+from auth import get_current_active_user
+from auth_schema import User
 from database import get_db
 
 router = APIRouter()
 
 
 @router.get("/get_meal_plan_by_id")
-async def get_plan_by_id(meal_plan_id: int, db: Session = Depends(get_db)):
+async def get_plan_by_id(meal_plan_id: int, db: Session = Depends(get_db),
+                         current_user: User = Depends(get_current_active_user)):
+    if current_user.role != "admin" and not current_user.is_active:
+        return {"status": 401, "details": "only admin can get plans"}
     return db.query(model.MealPlan).filter(model.MealPlan.meal_plan_id == meal_plan_id).first()
 
 
 @router.get("/get_recovery_plan_by_id")
-async def get_plan_by_id(recovery_plan_id: int, db: Session = Depends(get_db)):
+async def get_plan_by_id(recovery_plan_id: int, db: Session = Depends(get_db),
+                         current_user: User = Depends(get_current_active_user)):
+    if current_user.role != "admin" and not current_user.is_active:
+        return {"status": 401, "details": "only admin can get plans"}
     return db.query(model.RecoveryPlan).filter(model.RecoveryPlan.recovery_plan_id == recovery_plan_id).first()
