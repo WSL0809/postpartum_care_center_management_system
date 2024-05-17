3e93dfa16bfe72b01d99f3b4029496b20250df63
WangSL <48207171+WSL0809@users.noreply.github.com>
2024-05-17 09:59:14 +0800

    bugfix

--- api/get_plan_by_id.py
+++ api/get_plan_by_id.py
@@ -9,8 +9,9 @@ router = APIRouter()
 
 @router.get("/get_meal_plan_by_id")
 async def get_plan_by_id(meal_plan_id: int, db: Session = Depends(get_db)):
-    return db.query(model.MealPlan).filter(model.MealPlan.id == meal_plan_id).first()
+    return db.query(model.MealPlan).filter(model.MealPlan.meal_plan_id == meal_plan_id).first()
+
 
 @router.get("/get_recovery_plan_by_id")
 async def get_plan_by_id(recovery_plan_id: int, db: Session = Depends(get_db)):
-    return db.query(model.RecoveryPlan).filter(model.RecoveryPlan.id == recovery_plan_id).first()
+    return db.query(model.RecoveryPlan).filter(model.RecoveryPlan.recovery_plan_id == recovery_plan_id).first()
