e0efb7dcba7b6b9d846284abd8a3c51d35a280d5
WangSL <wangshilong0809@outlook.com>
2024-05-19 14:47:51 +0800

    update_client_status.py

--- api/__init__.py
+++ api/__init__.py
@@ -15,3 +15,4 @@ from .insert_client import router as insert_client_router
 from .manage_plan import router as manage_plan_router
 from .allocate_room import router as allocate_room_router
 from .get_plan_by_id import router as get_plan_by_id_router
+from .update_client_status import router as update_client_status_router
