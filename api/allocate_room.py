84b90e7ab97afc7da938ad45e44ea61284e9cb20
WangSL <wangshilong0809@outlook.com>
2024-05-26 18:14:57 +0800

    bugfix allocate_room.py

--- api/allocate_room.py
+++ api/allocate_room.py
@@ -35,12 +35,12 @@ async def allocate_room_by_client_id(client_id: int, room_number: str, current_u
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="此用户并非等待分配的用户，无法分配房间",
         )
-
-    if client.room is not None:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="此用户已分配房间",
-        )
+    #
+    # if client.room is not None:
+    #     raise HTTPException(
+    #         status_code=status.HTTP_400_BAD_REQUEST,
+    #         detail="此用户已分配房间",
+    #     )
     check_room_status = text(
         """
         SELECT status FROM room WHERE room_number = :room
